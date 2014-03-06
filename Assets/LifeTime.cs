cb5f3df004fd6536e9202743ba2c6594a4ef9a72
Daxaker <itjustsoap@gmail.com>
2014-03-07 01:20:40 +0400

    init commit

---
+++ Assets/LifeTime.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeTime : MonoBehaviour {
+
+	// Use this for initialization
+
+	int time =100;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(time--<=0){
+			Destroy(gameObject);
+		this.enabled = false;
+		}
+	}
+}
---
+++ Assets/ParachuteGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ParachuteGenerator : MonoBehaviour {
+
+	public GameObject parachuter;
+	public Camera cam;
+	// Use this for initialization
+	float intervalTimer;
+	float interval;
+	void Start(){
+		intervalTimer=Time.time;
+		interval = Random.Range(0,.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(intervalTimer+interval<=Time.time)
+		{
+			intervalTimer = Time.time;
+			interval = Random.Range(0,.5f);
+			parachuter.transform.position = new Vector2(Random.Range(cam.ScreenToWorldPoint(new Vector2(0,0)).x,cam.ScreenToWorldPoint(new Vector2(Screen.width,0)).x),parachuter.transform.position.y);
+			Instantiate(parachuter);
+		}
+	}
+}
