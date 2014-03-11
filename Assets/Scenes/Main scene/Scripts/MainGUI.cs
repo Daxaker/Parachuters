75d89d41f23884a341026d7ee855318c2142b745
Daxaker <itjustsoap@gmail.com>
2014-03-12 02:02:13 +0400

    fix gui

--- Assets/Scenes/Main scene/Scripts/MainGUI.cs
+++ Assets/Scenes/Main scene/Scripts/MainGUI.cs
@@ -6,25 +6,27 @@ public class MainGUI : MonoBehaviour {
 	public GUIStyle LabelStyle;
 
 	void OnGUI(){
-		GUI.Label(new Rect(20,5,Screen.width,Screen.height),string.Format("{0}",Game.LandedParachuters),LabelStyle);
+
 		if(Game.IsGameOver){
 			//Draw gameover gui
-			if(GUI.Button(new Rect(Screen.width*.25f,Screen.height*.4f,Screen.width*.5f,Screen.height*.1f),"Repeat")){
-				Reload(Application.loadedLevelName);
+			GUI.Label(new Rect(Screen.width*.1f,Screen.height*.1f,Screen.width,Screen.height),string.Format("Score:{0}\nBest:{1}",Game.LandedParachuters,Game.Record),LabelStyle);
+			if(GUI.Button(new Rect(Screen.width*.25f,Screen.height*.4f,Screen.width*.5f,Screen.height*.1f),"Replay")){
+				Replay();
 			}
 			if(GUI.Button(new Rect(Screen.width*.25f,Screen.height*.4f+Screen.height*.15f,Screen.width*.5f,Screen.height*.1f),"Rage quit!!!!")){
 				Application.Quit();
 			}
+
 		}
-		if(GUI.Button(new Rect(Screen.width-40,25,20,20),"||"))
+		else
 		{
-			Game.InvertPause();
+			GUI.Label(new Rect(Screen.width*.025f,5,Screen.width,Screen.height),string.Format("{0}",Game.LandedParachuters),LabelStyle);
 		}
 	}
 		
-	void Reload(string level)
+	void Replay()
 	{
 		Game.Reset();
-		Application.LoadLevel(level);
+		Application.LoadLevel(Application.loadedLevelName);
 	}
 }
