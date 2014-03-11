79320e3a56fd5a20ee0734aee9ac275cf7af80c4
Daxaker <itjustsoap@gmail.com>
2014-03-12 01:43:44 +0400

    reduce gravity scale

--- Assets/Scenes/Main scene/Scripts/MainGUI.cs
+++ Assets/Scenes/Main scene/Scripts/MainGUI.cs
@@ -12,8 +12,8 @@ public class MainGUI : MonoBehaviour {
 			if(GUI.Button(new Rect(Screen.width*.25f,Screen.height*.4f,Screen.width*.5f,Screen.height*.1f),"Repeat")){
 				Reload(Application.loadedLevelName);
 			}
-			if(GUI.Button(new Rect(Screen.width*.25f,Screen.height*.4f+Screen.height*.15f,Screen.width*.5f,Screen.height*.1f),"Main menu")){
-				Reload ("MainMenu");
+			if(GUI.Button(new Rect(Screen.width*.25f,Screen.height*.4f+Screen.height*.15f,Screen.width*.5f,Screen.height*.1f),"Rage quit!!!!")){
+				Application.Quit();
 			}
 		}
 		if(GUI.Button(new Rect(Screen.width-40,25,20,20),"||"))
